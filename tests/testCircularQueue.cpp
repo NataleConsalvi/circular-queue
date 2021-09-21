a94a4764d9e5cd57783b3b296edaf3d5b3f503ac
Natale <nataleconsalvi06@gmail.com>
2021-09-21 16:34:39 +0200

    add test 1 v2

--- tests/testCircularQueue.cpp
+++ tests/testCircularQueue.cpp
@@ -9,16 +9,3 @@ TEST_CASE("Test 1: capacity < 0 ", "[circular_queue]") {
 
     REQUIRE_THROWS_AS(Nats::CircularQueue(-1), std::invalid_argument);
 }
-
-/*
-TEST_CASE("square_root >= 0 should give positive result ", "[cxx_examples]") {
-                
-    REQUIRE(square_root(4.0) == Approx(2.0));
-
-
-    // expects std::invalid_argument exception, if it is not thrown test fails        
-    REQUIRE_THROWS_AS( square_root(-1.0), std::invalid_argument);
-
-    REQUIRE(square_root(0.0) == Approx(0.0));
-}
-*/
